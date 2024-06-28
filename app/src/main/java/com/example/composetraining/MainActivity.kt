46a5babb6ebed409a065fcd9191e8c00120a0fde
Peng Chen <pchen@thoughtworks.com>
2024-06-28 17:46:36 +0800

    finished course 8

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -4,8 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +30,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
-
+    LazyColumn {
+        itemsIndexed(
+            listOf("this", "is", "a", "jetpack", "compose")
+        ) { index, item ->
+            Text(
+                text = "Item $item, index: $index",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+//        items(5000) {
+//            Text(
+//                text = "Item $it",
+//                fontSize = 24.sp,
+//                fontWeight = FontWeight.Bold,
+//                textAlign = TextAlign.Center,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(vertical = 24.dp)
+//            )
+//        }
+    }
 }
 
 @Preview(showBackground = true)
