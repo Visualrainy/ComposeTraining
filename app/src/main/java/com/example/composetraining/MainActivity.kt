6147218f713071f2e813b0760766cfb58630e361
Peng Chen <pchen@thoughtworks.com>
2024-06-30 13:50:41 +0800

    finished course 9

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -4,8 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +32,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
+    val constraintSet = ConstraintSet {
+        val greenBox = createRefFor("greenBox")
+        val redBox = createRefFor("redBox")
+        val guideLine = createGuidelineFromTop(0.5f)
 
+        constrain(greenBox) {
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+
+        constrain(redBox) {
+            top.linkTo(guideLine)
+            start.linkTo(greenBox.end)
+            end.linkTo(parent.end)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+        createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Spread)
+    }
+    ConstraintLayout(constraintSet, Modifier.fillMaxSize()) {
+        Box(modifier = Modifier
+            .background(Color.Green)
+            .layoutId("greenBox")
+        )
+
+        Box(modifier = Modifier
+            .background(Color.Red)
+            .layoutId("redBox")
+        )
+    }
 }
 
 @Preview(showBackground = true)
