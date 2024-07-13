76d1f2a8efb3de0e99127e5b33878a15e70010a4
Peng Chen <pchen@thoughtworks.com>
2024-07-13 11:45:49 +0800

    finish Drop Down with 3D animation

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -4,8 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +51,88 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
 fun ContentView() {
+    Surface(
+        color = Color(0xff101010),
+        modifier = Modifier
+            .fillMaxSize()
 
+    ) {
+        DropDown(title = "Hello", modifier = Modifier.padding(15.dp).padding(top = 30.dp)) {
+            Text(
+                text = "World",
+                color = Color.White,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .background(Color.Green)
+                    .padding(10.dp)
+
+            )
+        }
+    }
+}
+
+@Composable
+fun DropDown(
+    title: String,
+    modifier: Modifier = Modifier,
+    initiallyOpened: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    var isOpen by remember {
+        mutableStateOf(initiallyOpened)
+    }
+
+    val alpha = animateFloatAsState(
+        targetValue = if (isOpen) 1f else 0f, label = "alpha", animationSpec = tween(
+            durationMillis = 300
+        )
+    )
+
+    val rotateX = animateFloatAsState(
+        targetValue = if (isOpen) 0f else -90f, label = "rotate x", animationSpec = tween(
+            durationMillis = 300
+        )
+    )
+
+    Column(modifier = modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = title, color = Color.White, fontSize = 16.sp)
+            Icon(imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "drop down",
+                tint = Color.White,
+                modifier = Modifier
+                    .rotate(if (isOpen) 0f else 180f)
+                    .clickable {
+                        isOpen = !isOpen
+                    })
+        }
+        Spacer(
+            modifier = Modifier
+                .height(10.dp)
+        )
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .graphicsLayer {
+                    transformOrigin = TransformOrigin(0.5f, 0f)
+                    rotationX = rotateX.value
+                }
+                .alpha(alpha.value)
+        ) {
+            content()
+        }
+    }
 }
 
 @Preview(showBackground = true)
