c3cd3ab77c8d4553cecc5dd9dd0c0e455f278e5e
Peng Chen <pchen@thoughtworks.com>
2024-07-16 09:45:33 +0800

    finish animation SplashScreen

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.composetraining
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +40,49 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff202020))
+    ) {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "splash_screen") {
+            composable(route = "splash_screen") {
+                SplashScreen(navController = navController)
+            }
+            composable(route = "main_screen") {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "Main Screen")
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = animateFloatAsState(
+        targetValue = 1.3f,
+        animationSpec = tween(
+            durationMillis = 1000,
+            easing = { fraction ->
+                OvershootInterpolator(2f).getInterpolation(fraction)
+            }
+        ), label = ""
+    )
+
+    LaunchedEffect(key1 = true) {
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.starbucks),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value)
+        )
+    }
 }
 
 @Preview(showBackground = true)
