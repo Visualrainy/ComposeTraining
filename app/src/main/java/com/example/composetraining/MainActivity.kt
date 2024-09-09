98a463b63f84c604d71184b34f1c68ab8fed7b24
Peng Chen <pchen@thoughtworks.com>
2024-09-09 14:53:49 +0800

    finish project for support all screens

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -30,29 +31,58 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(10) {
-            Text(
-                text = "Item $it",
-                fontSize = 25.sp,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Cyan)
-                    .padding(16.dp)
-            )
+    val windowInfo = rememberWindowInfo()
+    if (windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(10) {
+                Text(
+                    text = "Item $it",
+                    fontSize = 25.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Cyan)
+                        .padding(16.dp)
+                )
+            }
+            items(10) {
+                Text(
+                    text = "Item $it",
+                    fontSize = 25.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Green)
+                        .padding(16.dp)
+                )
+            }
         }
-        items(10) {
-            Text(
-                text = "Item $it",
-                fontSize = 25.sp,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Green)
-                    .padding(16.dp)
-            )
+    } else {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            LazyColumn(modifier = Modifier.weight(1f)) {
+                items(10) {
+                    Text(
+                        text = "Item $it",
+                        fontSize = 25.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.Cyan)
+                            .padding(16.dp)
+                    )
+                }
+            }
+            LazyColumn(modifier = Modifier.weight(1f)) {
+                items(10) {
+                    Text(
+                        text = "Item $it",
+                        fontSize = 25.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.Green)
+                            .padding(16.dp)
+                    )
+                }
+            }
         }
     }
-
 }
 
 @Preview(showBackground = true)
