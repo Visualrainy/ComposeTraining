dae7d6a776fd4f9a82811f2a7bd8198896830948
Peng Chen <pchen@thoughtworks.com>
2024-09-09 14:39:22 +0800

    init project for support all screens

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -4,8 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +30,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(10) {
+            Text(
+                text = "Item $it",
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Cyan)
+                    .padding(16.dp)
+            )
+        }
+        items(10) {
+            Text(
+                text = "Item $it",
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Green)
+                    .padding(16.dp)
+            )
+        }
+    }
 
 }
 
