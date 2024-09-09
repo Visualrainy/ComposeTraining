8969a30f0b9af6a0f5c79a90f73782107dda4184
Peng Chen <pchen@thoughtworks.com>
2024-09-09 10:53:49 +0800

    finish local spacing using LocalSpacing

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -4,23 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
+import com.example.composetraining.ui.theme.LocalSpacing
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ContentView()
+            ComposeTrainingTheme {
+                ContentView()
+            }
         }
     }
 }
 
 @Composable
 fun ContentView() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(LocalSpacing.current.medium)
+            .background(Color.Red)
+    ) {
 
+    }
 }
 
 @Preview(showBackground = true)
