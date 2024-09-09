7df66ba17535297da41b140b086ef301871132ef
Peng Chen <pchen@thoughtworks.com>
2024-09-09 16:35:44 +0800

    finish the motion layout in compose

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -4,8 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +50,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
+    Column {
+        var progress by remember {
+            mutableFloatStateOf(0f)
+        }
+        ProfileHeader(progress = progress)
+        Spacer(modifier = Modifier.height(32.dp))
+        Slider(value = progress, onValueChange = {
+            progress = it
+        }, modifier = Modifier.padding(horizontal = 32.dp))
+    }
+}
 
+@OptIn(ExperimentalMotionApi::class)
+@Composable
+fun ProfileHeader(progress: Float) {
+    val context = LocalContext.current
+    val motionScene = remember {
+        context.resources
+            .openRawResource(R.raw.motion_scene)
+            .readBytes()
+            .decodeToString()
+    }
+    MotionLayout(
+        motionScene = MotionScene(content = motionScene),
+        progress = progress,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.DarkGray)
+                .layoutId("box")
+        )
+        Image(
+            painter = painterResource(id = R.drawable.starbucks),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(CircleShape)
+                .border(2.dp, color = Color.Green, shape = CircleShape)
+                .layoutId("profile_pic")
+        )
+        Text(
+            text = "Chris",
+            fontSize = 24.sp,
+            modifier = Modifier.layoutId("username")
+        )
+    }
 }
 
 @Preview(showBackground = true)
