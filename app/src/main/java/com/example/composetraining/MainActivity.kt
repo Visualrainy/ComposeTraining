75c5843e0fa5bd0b727b5ebd1c8a8f47aa32909c
Peng Chen <pchen@thoughtworks.com>
2024-09-10 15:57:33 +0800

    finish lazy grid in compose

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -4,8 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +35,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
-
+    val state = rememberLazyGridState(initialFirstVisibleItemIndex = 99)
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(5),
+        state = state
+    ) {
+        items(100) { i ->
+            Box(
+                modifier = Modifier
+                    .aspectRatio(1f)
+                    .padding(8.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(Color.Green),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Item $i")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
