1933b9fe36d38c654c7091d1156838aea7285272
Peng Chen <pchen@thoughtworks.com>
2024-09-10 13:14:40 +0800

    finish navigation drawer in compose

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -4,9 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +38,57 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
-
+    val drawerState = rememberDrawerState(DrawerValue.Open)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                DrawerHeader()
+                DrawerBody(
+                    items = listOf(
+                        MenuItem(
+                            id = "Home",
+                            title = "Home",
+                            contentDescription = "Go to home screen",
+                            icon = Icons.Default.Home
+                        ),
+                        MenuItem(
+                            id = "Settings",
+                            title = "Settings",
+                            contentDescription = "Go to settings screen",
+                            icon = Icons.Default.Settings
+                        ),
+                        MenuItem(
+                            id = "Help",
+                            title = "Help",
+                            contentDescription = "Get help",
+                            icon = Icons.Default.Info
+                        )
+                    ),
+                    onItemClick = {
+                        scope.launch {
+                            drawerState.close()
+                        }
+                        println("Clicked on ${it.title}")
+                    }
+                )
+            }
+        }) {
+        Scaffold(
+            topBar = {
+                AppBar(onNavigationIconClick = {
+                    scope.launch {
+                        if (drawerState.isOpen) drawerState.close() else drawerState.open()
+                    }
+                })
+            }
+        ) { _ ->
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "Navigation Drawer Demo", fontSize = 36.sp)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
