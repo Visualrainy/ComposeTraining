89e1982f1a9c7bae1715a6988c5ef884d9d8f222
Peng Chen <pchen@thoughtworks.com>
2024-09-11 10:51:32 +0800

    finish app links in compose

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.composetraining
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +33,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
-
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {
+                    navController.navigate("detail")
+                }) {
+                    Text(text = "To detail")
+                }
+            }
+        }
+        composable(
+            route = "detail",
+            deepLinks = listOf(
+                navDeepLink {
+                    uriPattern = "https://pl-coding.com/{id}"
+                    action = Intent.ACTION_VIEW
+                }
+            ),
+            arguments = listOf(
+                navArgument("id") {
+                    type = NavType.IntType
+                    defaultValue = -1
+                }
+            )
+        ) {
+            val id = it.arguments?.getInt("id")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "this id is $id")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
