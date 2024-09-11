2bf7dfa4a1d665d590c7bb912817c1d69d2e2b6a
Peng Chen <pchen@thoughtworks.com>
2024-09-11 16:01:17 +0800

    finish animated counter text

--- app/src/main/java/com/example/composetraining/MainActivity.kt
+++ app/src/main/java/com/example/composetraining/MainActivity.kt
@@ -4,7 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetraining.ui.theme.ComposeTrainingTheme
 
@@ -20,7 +32,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContentView() {
-
+    Column(
+        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var count by remember {
+            mutableIntStateOf(0)
+        }
+        AnimatedCounter(count = count, style = MaterialTheme.typography.bodyLarge)
+        Button(onClick = { 
+            count++
+        }) {
+            Text(text = "Increment")
+        }
+        
+    }
 }
 
 @Preview(showBackground = true)
